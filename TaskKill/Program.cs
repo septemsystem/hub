76c38160362ad8a72f8b8d9b2b1ce9202f63ca3c
Павел Чертыковцев <7system83@mail.ru>
2020-11-25 00:46:14 +0400

    1

--- TaskKill/Program.cs
+++ TaskKill/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,27 @@ namespace TaskKill
     {
         static void Main(string[] args)
         {
+            DateTime date1 = new DateTime();
+            DateTime date2 = new DateTime(2020, 11, 24, 23, 0, 0);
+            int result = DateTime.Compare(date1, date2);
+
+            Console.WriteLine("12");
+
+            if (result > 0)
+
+            {
+                Process[] processes = Process.GetProcessesByName("clickermann");
+                foreach (Process process in processes)
+                {
+                    process.Kill();
+                }
+            }
+
+            else
+            {
+                Environment.Exit(0);
+            } 
+           
         }
     }
 }
